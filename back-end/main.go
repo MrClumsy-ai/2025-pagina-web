537a19ab70e9fd6750fc31fb32417d4c1e1c85dd
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-12 09:49:04 -0600

    modificado backend, creado inicio

--- back-end/main.go
+++ back-end/main.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"database/sql"
-	"net/http"
+	"html/template"
+	"io"
 
 	"github.com/labstack/echo/v4"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	tmpl := template.Must(template.ParseGlob("views/*.tmpl"))
+	tmpl = template.Must(tmpl.ParseGlob("views/partials/*.tmpl"))
+	return &Templates{
+		templates: tmpl,
+	}
+}
+
+type Counter struct {
+	Count int
+}
+
 func main() {
 	e := echo.New()
+	e.Static("/assets", "assets")
+	e.Renderer = newTemplate()
 	dbConnection, err := sql.Open("sqlite3", "pagina.db")
 	if err != nil {
 		e.Logger.Fatal("error connecting to db: ", err)
 	}
 	defer dbConnection.Close()
 
+	count := Counter{Count: 0}
+
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, `<h1>Welcome</h1>`)
+		count.Count++
+		return c.Render(200, "inicio", count)
 	})
 	e.Logger.Fatal(e.Start(":8000"))
 }
