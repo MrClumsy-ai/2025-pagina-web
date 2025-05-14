190953c614f009d30405964067d5f055b97d4318
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-14 16:10:04 -0600

    bug fix ver detalle

--- assets/mascotas.js
+++ assets/mascotas.js
@@ -1,7 +1,19 @@
-function mostrarModal() {
-  document.getElementById("detalleMascota").style.display = "flex";
-}
-
-function cerrarModal() {
-  document.getElementById("detalleMascota").style.display = "none";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const verDetalles = document.querySelectorAll(".ver-detalles");
+  const ocultarDetalles = document.querySelectorAll(".ocultar-detalles");
+  verDetalles.forEach((button) => {
+    button.addEventListener("click", () => {
+      const targetDiv = button.closest("article").nextElementSibling;
+      if (targetDiv.classList.contains("hidden")) {
+        targetDiv.classList.replace("hidden", "flex");
+      }
+    });
+  });
+  ocultarDetalles.forEach((button) => {
+    button.addEventListener("click", () => {
+      const targetDiv = button
+        .closest(".modal-mascota")
+        .classList.replace("flex", "hidden");
+    });
+  });
+});
