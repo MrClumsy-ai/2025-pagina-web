9ee047a3fcabc6e67e2d76c7f09d135d4f758f03
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-13 23:27:32 -0600

    creado api basico

--- back-end/main.go
+++ back-end/main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"net/http"
+	"strconv"
 
-	// "encoding/base64"
 	"html/template"
 	"io"
-	"proweb-backend/database"
 
 	"github.com/labstack/echo/v4"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const PORT = "8000"
+
 type Templates struct {
 	templates *template.Template
 }
+
+type Mascota struct {
+	Id          int
+	Nombre      string
+	Edad        int
+	Altura_cm   int
+	Foto        []byte
+	Foto64      string
+	Descripcion string
+}
+
+type Mascotas []Mascota
 
 func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
@@ -39,18 +51,18 @@ func main() {
 		e.Logger.Fatal("error connecting to db: ", err)
 	}
 	defer dbConnection.Close()
-	mascotaRepository := &database.MascotaRepository{Db: dbConnection}
-	err = mascotaRepository.CreateTable()
+	_, err = dbConnection.Exec(`CREATE TABLE IF NOT EXISTS mascotas (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		nombre TEXT,
+		edad INTEGER,
+		altura_cm INTEGER,
+		foto BLOB,
+		descripcion TEXT)`)
 	if err != nil {
-		e.Logger.Fatal("error creating mascota table: ", err)
+		e.Logger.Fatal("error creating mascota table", err)
 	}
 
 	e.GET("/", func(c echo.Context) error {
-		/* foto, err := os.ReadFile("assets/img/splash.jpg")
-		if err != nil {
-			e.Logger.Fatal("failed to load file")
-		}
-		base64str := base64.StdEncoding.EncodeToString(foto) */
 		e.Logger.Printf("retrieved file")
 		response := map[string]any{
 			"CurrentRoute": "/",
@@ -81,16 +93,68 @@ func main() {
 		return c.Render(http.StatusOK, "contacto", response)
 	})
 
+	// API
+
 	e.GET("/api/mascotas", func(c echo.Context) error {
-		res, err := mascotaRepository.GetAll()
+		rows, err := dbConnection.Query("SELECT * FROM mascotas")
 		if err != nil {
-			e.Logger.Fatal("error getting all", err)
-			return c.JSON(http.StatusInternalServerError, err)
+			e.Logger.Fatal(err)
+			return c.JSON(http.StatusInternalServerError, nil)
 		}
-		r, err := json.Marshal(res)
-		e.Logger.Printf("%v", r)
-		return c.JSON(http.StatusOK, r)
+		defer rows.Close()
+		var mascotas Mascotas
+		for rows.Next() {
+			var mascota Mascota
+			err := rows.Scan(&mascota.Id, &mascota.Nombre, &mascota.Edad, &mascota.Altura_cm, &mascota.Foto, &mascota.Descripcion)
+			if err != nil {
+				e.Logger.Fatal(err)
+				return c.JSON(http.StatusInternalServerError, nil)
+			}
+			mascotas = append(mascotas, mascota)
+		}
+		if len(mascotas) == 0 {
+			return c.JSON(http.StatusNotFound, nil)
+		}
+		return c.JSON(http.StatusFound, mascotas)
 	})
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.GET("/api/mascotas/:id", func(c echo.Context) error {
+		var mascota Mascota
+		pId, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			e.Logger.Fatal(err)
+			return c.JSON(http.StatusInternalServerError, nil)
+		}
+		err = dbConnection.QueryRow("SELECT * FROM mascotas WHERE ID = ?", pId).Scan(&mascota.Id, &mascota.Nombre, &mascota.Edad, &mascota.Altura_cm, &mascota.Foto, &mascota.Descripcion)
+		if err != nil {
+			return c.JSON(http.StatusNotFound, "mascota not found")
+		}
+		return c.JSON(http.StatusFound, mascota)
+	})
+
+	e.POST("/api/mascotas", func(c echo.Context) error {
+		var reqBody Mascota
+		if err := c.Bind(&reqBody); err != nil {
+			return c.JSON(http.StatusBadRequest, "Bad request")
+		}
+		e.Logger.Printf("request body: %v", reqBody)
+		if reqBody.Nombre == "" {
+			return c.JSON(http.StatusBadRequest, "No name given")
+		}
+		_, err = dbConnection.Exec("INSERT INTO mascotas (name) VALUES (?)", reqBody.Name)
+		if err != nil {
+			e.Logger.Fatal(err)
+			return c.JSON(http.StatusInternalServerError, nil)
+		}
+		log.Println("inserted into db")
+		return c.JSON(http.StatusCreated, reqBody.Name)
+	})
+
+	/* foto, err := os.ReadFile("assets/img/splash.jpg")
+	if err != nil {
+		e.Logger.Fatal("failed to load file")
+	}
+	base64str := base64.StdEncoding.EncodeToString(foto) */
+
+	e.Logger.Fatal(e.Start(":" + PORT))
 }
